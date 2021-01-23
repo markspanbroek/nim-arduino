015624000708191a118348084336043fc7c06d17
Mark Spanbroek <mark@spanbroek.net>
2021-01-23 14:08:20 +0100

    Add most of the other Arduino functions

--- arduino.nim
+++ arduino.nim
@@ -13,17 +13,34 @@ type
   Pin* = distinct uint8
   Mode* = distinct uint8
   Value* = distinct uint8
+  BitOrder* = distinct uint8
 
 {.push importc, header:"Arduino.h".}
 
 let LED_BUILTIN* {.nodecl.}: Pin
 let INPUT* {.nodecl.}: Mode
 let OUTPUT* {.nodecl.}: Mode
+let INPUT_PULLUP* {.nodecl.}: Mode
 let HIGH* {.nodecl.}: Value
 let LOW* {.nodecl.}: Value
+let LSBFIRST* {.nodecl.}: BitOrder
+let MSBFIRST* {.nodecl.}: BitOrder
 
-proc delay*(milliseconds: int)
+proc delay*(milliseconds: culong)
+proc delayMicroseconds*(microseconds: cuint)
+proc millis*: culong
+proc micros*: culong
 proc pinMode*(pin: Pin, mode: Mode)
 proc digitalWrite*(pin: Pin, value: Value)
+proc digitalRead*(pin: Pin): Value
+proc analogRead*(pin: Pin): cint
+proc analogReference*(mode: uint8)
+proc analogWrite*(pin: Pin, value: cint)
+proc tone*(pin: Pin, frequency: cint, duration: culong = 0)
+proc noTone*(pin: Pin)
+proc pulseIn*(pin: Pin, value: Mode, timeout: culong = 1000000'u): culong
+proc pulseInLong*(pin: Pin, value: Mode, timeout: culong = 1000000'u): culong
+proc shiftIn*(data, clock: Pin, order: BitOrder): uint8
+proc shiftOut*(data, clock: Pin, order: BitOrder, value: uint8)
 
 {.pop.}
